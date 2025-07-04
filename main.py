ed98d33ffa1e1e9fffe8303c87abfb7c04f41e67
mpmk <parent.mathieu10@protonmail.com>
2025-07-03 20:38:22 -0400

    Refactored main.py file

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    iteration = 0
-    while iteration == 0:
+    while True:
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           return
-      pygame.Surface.fill(screen, (0,0,0))
+      screen.fill("black")
       pygame.display.flip()
 
 if __name__ == "__main__":
