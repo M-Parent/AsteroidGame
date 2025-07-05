4d7b00b7cc95f2bd827759f62f61a00982c8fe4c
mpmk <parent.mathieu10@protonmail.com>
2025-07-04 20:02:16 -0400

    Player hit asteroid game over logic done

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import pygame
+import sys
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable, drawable)
+
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     while True:
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           return
+        
+      updatable.update(dt)
+      
+      for asteroid in asteroids:
+         if asteroid.collides_with(player):
+            print("Game over!")
+            sys.exit(1)
 
-      player.update(dt)
 
       screen.fill("black")
-      player.draw(screen)
+      for obj in drawable:
+         obj.draw(screen)
+     
       pygame.display.flip()
 
       # limit the framerate to 60 fps
